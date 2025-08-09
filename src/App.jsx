8215216f5247139105ce3b22b62c8e821be577c9
Harshita Mudigonda <harshitamudigonda@gmail.com>
2025-08-09 10:31:20 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S105</h1>
       <h2>CI/CD Cloud Devops - Section 105</h2>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <h1>Done with Git Pages & Git Actions</h1>
     </div>
   );
 }
